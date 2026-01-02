bde75d38223c76a82229d1930c24d2ef6dda4fcd
AnkitRawat012 <ankit.bhore0@gmail.com>
2026-01-02 15:13:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node js not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Ankit")
+
+let name = "Ankit"
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// premitive datatype
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Hitesh")
+console.log(typeof age);
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
