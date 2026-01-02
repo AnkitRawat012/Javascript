41e8c666c7534dea95a2010ac6c75e760112c676
AnkitRawat012 <ankit.bhore0@gmail.com>
2026-01-02 15:34:14 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abcd"
+// let score = null
+// let score = undefined
+// let score = true
+let score ="Ankit "
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// Notes
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Ankit"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "Ankit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
