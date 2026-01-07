61395acf9f914b6317ecd335015cea33fdc073b6
AnkitRawat012 <ankit.bhore0@gmail.com>
2026-01-07 21:12:37 +0000

    com func

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -37,4 +37,36 @@ function loginUserMessage(username){
 // console.log(login);
 
 // console.log(loginUserMessage('Bhore'));
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1 , val2 , ...number1){
+    return number1
+
+}
+// console.log(calculateCartPrice(2));
+
+console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "Bhore",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
+
+handleObject({
+    username: 'Shivam',
+    price : 399
+})
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+     return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,200,300,400]))
